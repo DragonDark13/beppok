d3965317318ccf3dfeb4ff2d1a4f396b99b2f3fa
DragonDark13 <aleksdark13@gmail.com>
2017-01-09 22:28:08 +0200

    beppok_responsive

--- javascript/my_script.js
+++ javascript/my_script.js
@@ -1,26 +1,34 @@
 $(document).ready(function() {
 	
 
-	var myWidth = 0;
-        myWidth = window.innerWidth;
-        jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:999;color:#fff;">Width = '+myWidth+'</div>');
-        jQuery(window).resize(function(){
-            var myWidth = 0;
-            myWidth = window.innerWidth;
-            jQuery('#size').remove();
-            jQuery('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:999;color:#fff;">Width = '+myWidth+'</div>');
-        });
+	// var myWidth = 0;
+ //        myWidth = window.innerWidth;
+ //        $('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:999;color:#fff;">Width = '+myWidth+'</div>');
+ //        $(window).resize(function(){
+ //            var myWidth = 0;
+ //            myWidth = window.innerWidth;
+ //            $('#size').remove();
+ //            $('body').prepend('<div id="size" style="background:#000;padding:5px;position:fixed;z-index:999;color:#fff;">Width = '+myWidth+'</div>');
+ //        });
 
         
-	var mainMenu = jQuery('.main_meni');
+	var mainMenu = $('.main_meni');
 	mainMenu.find('ul').parent("li").addClass('parent')
 	mainMenu.find('li.parent > a').append('<span class="arrow"></span>');
 	mainMenu.find(' > li').last().addClass('lastChild');
 
 
   $('.bxslider').bxSlider({
-  	slideWidth: 620
+  	slideWidth: 620,
+    responsive: true
   });
+
+  $('.main_meni_header>i').click(function(event) {
+    /* Act on the event */
+    $(this).parent().toggleClass('open_meni');
+  });
+
+
 
 
 
